dbc9daf6f0d77495b7cd4fdb33897ff5f1e30917
Rudy Wahjudi <rudy.wahjudi@gmail.com>
2017-11-17 00:47:13 -0800

    created mongoose schemas of Car, CarDealer, FlightBilling, FlightBillingDetail, Logs

---
+++ server/models/mongooseCarDealer.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const carDealerSchema = new Schema({
+  name: { type: String, required: true },
+  address: { type: String, required: true },
+  city: { type: String, required: true },
+  state: { type: String, required: true },
+  country: { type: String, required: true },
+  zipcode: { type: String, required: true },
+  createdAt: { type: Date, required: false, default: Date.now },
+  updatedAt: { type: Date, required: false, default: Date.now },
+});
+
+const CarDealer = mongoose.model('CarDealer', carDealerSchema);
+
+module.exports = CarDealer;
