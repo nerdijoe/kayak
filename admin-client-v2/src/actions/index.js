90ba39b48990b70fdfc5159f67f5a571681bdedd
Na Yue <na.yue@sjsu.edu>
2017-11-23 09:12:35 -0800

    fix edit user listing url path

--- admin-client-v2/src/actions/index.js
+++ admin-client-v2/src/actions/index.js
@@ -185,7 +185,7 @@ export const axiosEditUser = (user) => (dispatch) => {
     //get admin token
     const id = user.id;
     console.log("before edit user, the id is : "+ id);
-    axios.put('http://localhost:3000/users/id', user)
+    axios.put(`http://localhost:3000/users/${id}`, user)
         .then((res) => {
             console.log('-------------- after axiosEditUser--------------');
             console.log(res.data);
