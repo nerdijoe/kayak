c52b77634ef772d9ed878dc8926da03b0e1de7d3
Rudy Wahjudi <rudy.wahjudi@gmail.com>
2017-11-25 11:34:00 -0800

    added a new page for car billing listing

--- admin-client-v2/src/routes/app.jsx
+++ admin-client-v2/src/routes/app.jsx
@@ -10,6 +10,7 @@ import Upgrade from 'views/Upgrade/Upgrade';
 import SignIn from 'views/Auth/SignIn';
 
 import CarList from 'views/Cars/CarList';
+import CarBillingList from 'views/CarBillings/CarBillingList';
 import UserList from 'views/Users/UserList';
 
 const appRoutes = [
@@ -22,6 +23,7 @@ const appRoutes = [
     { path: "/maps", name: "Maps", icon: "pe-7s-map-marker", component: Maps },
     { path: "/notifications", name: "Notifications", icon: "pe-7s-bell", component: Notifications },
     { path: "/car", name: "Car Listing", icon: "pe-7s-car", component: CarList },
+    { path: "/carBilling", name: "Car Billings", icon: "pe-7s-car", component: CarBillingList },
     { path: "/signin", name: "Sign In", icon: "pe-7s-bell", component: SignIn },
     { path: "/userlist", name: "User Listing", icon: "pe-7s-car", component: UserList },
     // { upgrade: true, path: "/upgrade", name: "Upgrade to PRO", icon: "pe-7s-rocket", component: Upgrade },
