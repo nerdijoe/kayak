f59b2386376a38a9121a9bc7b9620d769107fbbc
Jiaqi Qin <frank.qjq@outlook.com>
2017-11-28 21:41:03 -0800

    1. DONE flight billing 2. DONE hotel billing

--- server/helpers/DBTool.js
+++ server/helpers/DBTool.js
@@ -4,3 +4,9 @@ exports.getCaseInsensitiveRegex = function(string){
   var regex = new RegExp(["^", string, "$"].join(""), "i");
   return regex;
 };
+
+const priceMap = new Map();
+priceMap.set("Business", 0);
+priceMap.set("Economy", 1);
+priceMap.set("First", 2);
+exports.priceMap = priceMap;
---
+++ server/models/mongooseHotelBilling.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const hotelBillingSchema = new Schema({
+  userId: { type: Number, required: true },
+  hotel: { type: Schema.Types.ObjectId, ref: 'Hotel', required: true },
+  room: { type: Schema.Types.ObjectId, ref: 'HotelRoom', required: true },
+  startDate: { type: Date, required: true },
+  endDate: { type: Date, required: true },
+  daysBooked: { type: Number, required: true },
+  qtyBooked: { type: Number, required: true },
+  priceBooked: { type: Number, required: true },
+  totalAmount: { type: Number, required: true },
+  isPaid: { type: Boolean, default: false },
+  isDeleted: { type: Boolean, default: false },
+  isCanceled: { type: Boolean, default: false },
+}, {
+  timestamps: true,
+});
+
+const HotelBilling = mongoose.model('HotelBilling', hotelBillingSchema);
+
+module.exports = HotelBilling;
