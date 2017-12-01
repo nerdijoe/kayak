f557383b0913426bd155d3221e2ac9783e0d1566
Rudy Wahjudi <rudy.wahjudi@gmail.com>
2017-11-30 23:17:51 -0800

    added kafka make request on server

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,5 +3,5 @@ const passport = require('passport');
 const authController = require('../controllers/auth');
 
 router.post('/signup', authController.signup);
-router.post('/signin', passport.authenticate('user', { session: false }), authController.signin);
+router.post('/signin', passport.authenticate('user', { session: false }), authController.signinKafka);
 module.exports = router;
