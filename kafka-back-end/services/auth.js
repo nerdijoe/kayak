5aca996c7a22fc33c288cbbdcdde33b5d73169de
Rudy Wahjudi <rudy.wahjudi@gmail.com>
2017-12-01 16:00:29 -0800

    disable kafka on server, so that others can still run the server. but it is already implemented for user signup and sigin

--- kafka-back-end/services/auth.js
+++ kafka-back-end/services/auth.js
@@ -11,38 +11,9 @@ module.exports = {
   signIn: (msg, cb) => {
     var res = {};
     console.log("In handle request, msg="+ JSON.stringify(msg));
-  
-    // if (msg.username == "bhavan@b.com" && msg.password =="a") {
-    //   console.log('*** handle_request user and pass correct');
-    //   res.code = "200";
-    //   res.value = "Success Login";
-    // } else {
-    //   console.log('*** handle_request failed login');
-    //   res.code = "401";
-    //   res.value = "Failed Login";
-    // }
-  
+    
     const username = msg.username;
-    const password = msg.password
-  
-    // User.findOne({
-    //   email: username,
-    // }, (err, user) => {
-    //   console.log('******** user=', user);
-    //   if (err) cb(err);
-    //   if (!user) {
-    //     cb(null, false, { message: 'User does not exist' });
-    //     // cb.json({ message: 'User does not exist'})
-    //   } else {
-    //     if (passwordHash.verify(password, user.password)) {
-    //       console.log('********* user password is verified, user=', user);
-    //       cb(null, user);
-    //     } else {
-    //       cb(null, false, {message: 'Password is not correct !'})
-    //       // cb(null, false);
-    //     }
-    //   }
-    // });
+    const password = msg.password;
 
     db.User.findOne({
       where: {
@@ -116,50 +87,7 @@ module.exports = {
     console.log('signUp msg=', msg);
     const req = msg;
     const data = req.body;
-    // data.password = passwordHash.generate(req.body.password);
   
-    // User.findOne({ email: data.email }, (err, user) => {
-    //   if (user) {
-    //     const errorMsg = {
-    //       message: 'Email is already used. Please sign up using different email.',
-    //     };
-    //     // res.json(errorMsg);
-    //     cb(false, errorMsg);
-    //   } else {
-    //     User.create({
-    //       firstname: data.firstname,
-    //       lastname: data.lastname,
-    //       email: data.email,
-    //       password: data.password,
-    //       // mysql_id: 0,
-    //     }, (err2, newUser) => {
-    //       if (err2) cb(false, err2); //res.json(err2);
-  
-    //       const newAbout = About({
-    //         overview: '',
-    //         work_edu: '',
-    //         contact_info: '',
-    //         life_events: '',
-    //         user: newUser._id,
-    //       });
-  
-    //       newAbout.save((err3, about) => {
-    //         const newInterest = Interest({
-    //           music: '',
-    //           shows: '',
-    //           sports: '',
-    //           fav_teams: '',
-    //           user: newUser._id,
-    //         });
-    //         newInterest.save((err4, interest) => {
-    //           // res.json(newUser);
-    //           cb(false, newUser);
-    //         });
-    //       }); // end of newAbout.save
-    //     });
-    //   } // end of else
-    // });
-
     db.User.findOne({
       where: {
         email: req.body.email,
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,6 +2,10 @@ const router = require('express').Router();
 const passport = require('passport');
 const authController = require('../controllers/auth');
 
-router.post('/signup', authController.signupKafka);
-router.post('/signin', passport.authenticate('user', { session: false }), authController.signinKafka);
+router.post('/signup', authController.signup);
+router.post('/signin', passport.authenticate('user', { session: false }), authController.signin);
+
+// router.post('/signup', authController.signupKafka);
+// router.post('/signin', passport.authenticate('kafka_user', { session: false }), authController.signinKafka);
+
 module.exports = router;
