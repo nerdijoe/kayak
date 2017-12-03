8f4b075a68eab084c9c4d0a1f22637770b48e87a
Manali Jain <jainmanali05@gmail.com>
2017-12-03 01:00:15 -0800

    my bookings integrated

--- kayak-client/src/components/carBookingList.js
+++ kayak-client/src/components/carBookingList.js
@@ -2,79 +2,37 @@
  * Created by ManaliJain on 11/24/17.
  */
 import React, {Component} from 'react';
-import {connect} from 'react-redux';
+import * as API from '../api/bookings';
 
 class CarBookingList extends Component{
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            carBookings:'',
+            message:''
+        }
     }
 
     render() {
 
-        const booking =  this.props.carBooking;
-        console.log("booking",booking);
+        let userToken = localStorage.getItem('user_token');
+        API.getCarBookings(userToken)
+            .then((res) => {
+                console.log("billing car response came111",res);
+                if(res.data !== null) {
+                    this.setState({
+                        message: res.data.message,
+                        carBookings: res.data
+                    });
+                }
+            }).catch( (error) => {
+            this.setState({
+                ...this.state
+            });
+        });
 
-        if(booking !== ''){
-            let sdate = booking.startDate.split("T");
-            let startDate = sdate[0];
-            let edate = booking.endDate.split("T");
-            let endDate = edate[0];
-            return (
-                <div>
-                    <ul className="myBooking-list ">
-                        <li className="bookedItem">
-                            <div className="booking-item-container">
-                                <div className="booking-item">
-                                    <div className="row">
-                                        <div className="starred-item__content starred-item__title col-sm-2">
-                                            <p className="booking-item-flight-className labelColorBooking">Place :</p>
-                                            <p className="booking-item-car-title">{booking.dealer.city}</p>
-                                        </div>
-                                        <div className="starred-item__content col-sm-1">
-                                            <p className="booking-item-flight-className labelColorBooking">Number of Days Booked :</p>
-                                            <p className="booking-item-car-title">{booking.daysBooked}</p>
-                                        </div>
-                                        <div className="starred-item__content col-sm-1">
-                                            <p className="booking-item-flight-className labelColorBooking">Model :</p>
-                                            <p className="booking-item-car-title">{booking.car.model}</p>
-                                        </div>
-                                        <div className="starred-item__content col-sm-1">
-                                            <p className="booking-item-flight-className labelColorBooking">Type :</p>
-                                            <p className="booking-item-car-title">{booking.car.type}</p>
-                                        </div>
-                                        <div className="starred-item__content col-sm-1">
-                                            <p className="booking-item-flight-className labelColorBooking">Pick-up by :</p>
-                                            <p className="booking-item-car-title">{startDate}</p>
-                                        </div>
-                                        <div className="starred-item__content col-sm-1">
-                                            <p className="booking-item-flight-className labelColorBooking">Drop by :</p>
-                                            <p className="booking-item-car-title">{endDate}</p>
-                                        </div>
-                                        <div className="starred-item__content col-sm-1">
-                                            <p className="booking-item-flight-className labelColorBooking">Price :</p>
-                                            <p className="booking-item-car-title">${booking.totalAmount}</p>
-                                        </div>
-                                        <div className="starred-item__content col-sm-1">
-                                            <p className="booking-item-flight-className labelColorBooking">Capacity :</p>
-                                            <p className="booking-item-car-title">{booking.car.capacity}</p>
-                                        </div>
-                                        <div className="starred-item__content col-sm-3">
-                                            <p className="booking-item-flight-className labelColorBooking">Dealer Address :</p>
-                                            <p className="booking-item-car-title">{booking.dealer.address}</p>
-                                            <p className="booking-item-car-title">{booking.dealer.city}</p>
-                                            <p className="booking-item-car-title">{booking.dealer.state} {booking.dealer.country}
-                                                 {booking.dealer.zipcode}</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-            );
-        }else {
-            return (
+        if(this.state.carBookings === ''){
+            return(
                 <div>
                     <div className = "row">
                         <ul className="starred-list">
@@ -86,9 +44,72 @@ class CarBookingList extends Component{
                         </ul>
                     </div>
                 </div>
+            )
+        } else{
+            return (
+                    <div>
+                        {this.state.carBookings.map((item, index) => {
+                            let booking = item;
+                                let sdate = booking.startDate.split("T");
+                                let startDate = sdate[0];
+                                let edate = booking.endDate.split("T");
+                                let endDate = edate[0];
+                            return (
+                                <ul className="myBooking-list ">
+                                             <li className="bookedItem">
+                                                 <div className="booking-item-container">
+                                                     <div className="booking-item">
+                                                         <div className="row">
+                                                             <div className="starred-item__content starred-item__title col-sm-2">
+                                                                 <p className="booking-item-flight-className labelColorBooking">Place :</p>
+                                                                 <p className="booking-item-car-title">{booking.dealer.city}</p>
+                                                             </div>
+                                                             <div className="starred-item__content col-sm-1">
+                                                                 <p className="booking-item-flight-className labelColorBooking">Number of Days Booked :</p>
+                                                                 <p className="booking-item-car-title">{booking.daysBooked}</p>
+                                                             </div>
+                                                             <div className="starred-item__content col-sm-1">
+                                                                 <p className="booking-item-flight-className labelColorBooking">Model :</p>
+                                                                 <p className="booking-item-car-title">{booking.car.model}</p>
+                                                             </div>
+                                                             <div className="starred-item__content col-sm-1">
+                                                                 <p className="booking-item-flight-className labelColorBooking">Type :</p>
+                                                                 <p className="booking-item-car-title">{booking.car.type}</p>
+                                                             </div>
+                                                             <div className="starred-item__content col-sm-1">
+                                                                 <p className="booking-item-flight-className labelColorBooking">Capacity :</p>
+                                                                 <p className="booking-item-car-title">{booking.car.capacity}</p>
+                                                             </div>
+                                                             <div className="starred-item__content col-sm-1">
+                                                                 <p className="booking-item-flight-className labelColorBooking">Pick-up by :</p>
+                                                                 <p className="booking-item-car-title">{startDate}</p>
+                                                             </div>
+                                                             <div className="starred-item__content col-sm-1">
+                                                                 <p className="booking-item-flight-className labelColorBooking">Drop by :</p>
+                                                                 <p className="booking-item-car-title">{endDate}</p>
+                                                             </div>
+                                                             <div className="starred-item__content col-sm-1">
+                                                                 <p className="booking-item-flight-className labelColorBooking">Price :</p>
+                                                                 <p className="booking-item-car-title">${booking.totalAmount}</p>
+                                                             </div>
+                                                             <div className="starred-item__content col-sm-3">
+                                                                 <p className="booking-item-flight-className labelColorBooking">Dealer Address :</p>
+                                                                 <p className="booking-item-car-title">{booking.dealer.address}</p>
+                                                                 <p className="booking-item-car-title">{booking.dealer.city}</p>
+                                                                 <p className="booking-item-car-title">{booking.dealer.state} {booking.dealer.country}
+                                                                      {booking.dealer.zipcode}</p>
+                                                             </div>
+                                                         </div>
+                                                     </div>
+                                                 </div>
+                                             </li>
+                                         </ul>
+                            );
+                        })}
+                    </div>
             );
-        }
 
+        }
     }
 }
 
--- kayak-client/src/components/hotelBookingList.js
+++ kayak-client/src/components/hotelBookingList.js
@@ -3,77 +3,36 @@
  */
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import * as API from '../api/bookings';
 
 class HotelBookingList extends Component{
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            hotelBookings:'',
+            message:''
+        }
     }
 
     render() {
+        let userToken = localStorage.getItem('user_token');
+        API.getHotelBookings(userToken)
+            .then((hotelRes) => {
+                console.log("billing hotel response came111",hotelRes);
+                if(hotelRes.data !== null){
+                    this.setState({
+                        message: hotelRes.data.message,
+                        hotelBookings: hotelRes.data
+                    });
+                }
+            }).catch( (error) => {
+            this.setState({
+                ...this.state
+            });
+        });
 
-        const hbooking =  this.props.hotelBooking;
-        console.log("booking",hbooking);
 
-        if(hbooking !== ''){
-            let sdate = hbooking.startDate.split("T");
-            let startDate = sdate[0];
-            let edate = hbooking.endDate.split("T");
-            let endDate = edate[0];
-            return (
-                <div>
-                    <ul className="myBooking-list ">
-                        <li className="bookedItem">
-                            <div className="booking-item-container">
-                                <div className="booking-item">
-                                    <div className="row">
-                                        <div className="starred-item__content starred-item__title col-sm-2">
-                                            <p className="booking-item-flight-className labelColorBooking">Hotel Name :</p>
-                                            <p className="booking-item-car-title">{hbooking.hotel.name}</p>
-                                        </div>
-                                        <div className="starred-item__content starred-item__title col-sm-1">
-                                            <p className="booking-item-flight-className labelColorBooking">Place :</p>
-                                            <p className="booking-item-car-title">{hbooking.hotel.city}</p>
-                                        </div>
-                                        <div className="starred-item__content col-sm-1">
-                                            <p className="booking-item-flight-className labelColorBooking">Number of Days Booked :</p>
-                                            <p className="booking-item-car-title">{hbooking.daysBooked}</p>
-                                        </div>
-                                        <div className="starred-item__content col-sm-1">
-                                            <p className="booking-item-flight-className labelColorBooking">Rooms Booked :</p>
-                                            <p className="booking-item-car-title">{hbooking.qtyBooked}</p>
-                                        </div>
-                                        <div className="starred-item__content col-sm-1">
-                                            <p className="booking-item-flight-className labelColorBooking">Rooms Type :</p>
-                                            <p className="booking-item-car-title">{hbooking.hotel.roomType}</p>
-                                        </div>
-                                        <div className="starred-item__content col-sm-1">
-                                            <p className="booking-item-flight-className labelColorBooking">Check in Date :</p>
-                                            <p className="booking-item-car-title">{startDate}</p>
-                                        </div>
-                                        <div className="starred-item__content col-sm-1 labelColorBooking">
-                                            <p className="booking-item-flight-className">Check out Date :</p>
-                                            <p className="booking-item-car-title">{endDate}</p>
-                                        </div>
-                                        <div className="starred-item__content col-sm-1">
-                                            <p className="booking-item-flight-className labelColorBooking">Price :</p>
-                                            <p className="booking-item-car-title">${hbooking.totalAmount}</p>
-                                        </div>
-                                        <div className="starred-item__content col-sm-3">
-                                            <p className="booking-item-flight-className labelColorBooking">Hotel Address :</p>
-                                            <p className="booking-item-car-title">{hbooking.hotel.address}</p>
-                                            <p className="booking-item-car-title">{hbooking.hotel.city}</p>
-                                            <p className="booking-item-car-title">{hbooking.hotel.state} {hbooking.hotel.country}
-                                                {hbooking.hotel.zipcode}</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-            );
-        }else {
+        if(this.state.hotelBookings === ''){
             return (
                 <div>
                     <div className = "row">
@@ -85,6 +44,82 @@ class HotelBookingList extends Component{
                             </li>
                         </ul>
                     </div>
+                </div>
+            );
+        } else {
+            return(
+                <div>
+                    {this.state.hotelBookings.map((item, index) => {
+                        let booking = item;
+                        let sdate = booking.startDate.split("T");
+                        let startDate = sdate[0];
+                        let edate = booking.endDate.split("T");
+                        let endDate = edate[0];
+                        return (
+                            <div>
+                                <ul className="myBooking-list ">
+                                    <li className="bookedItem">
+                                        <div className="booking-item-container">
+                                            <div className="booking-item">
+                                                <div className="row">
+                                                    <div className="starred-item__content starred-item__title col-sm-2">
+                                                        <p className="booking-item-flight-className labelColorBooking">
+                                                            Hotel Name :</p>
+                                                        <p className="booking-item-car-title">{booking.hotel.name}</p>
+                                                    </div>
+                                                    <div className="starred-item__content starred-item__title col-sm-1">
+                                                        <p className="booking-item-flight-className labelColorBooking">
+                                                            Place :</p>
+                                                        <p className="booking-item-car-title">{booking.hotel.city}</p>
+                                                    </div>
+                                                    <div className="starred-item__content col-sm-1">
+                                                        <p className="booking-item-flight-className labelColorBooking">
+                                                            Number of Days Booked :</p>
+                                                        <p className="booking-item-car-title">{booking.daysBooked}</p>
+                                                    </div>
+                                                    <div className="starred-item__content col-sm-1">
+                                                        <p className="booking-item-flight-className labelColorBooking">
+                                                            Rooms Booked :</p>
+                                                        <p className="booking-item-car-title">{booking.qtyBooked}</p>
+                                                    </div>
+                                                    <div className="starred-item__content col-sm-1">
+                                                        <p className="booking-item-flight-className labelColorBooking">
+                                                            Rooms Type :</p>
+                                                        <p className="booking-item-car-title">{booking.hotel.roomType}</p>
+                                                    </div>
+                                                    <div className="starred-item__content col-sm-1">
+                                                        <p className="booking-item-flight-className labelColorBooking">
+                                                            Check in Date :</p>
+                                                        <p className="booking-item-car-title">{startDate}</p>
+                                                    </div>
+                                                    <div className="starred-item__content col-sm-1 labelColorBooking">
+                                                        <p className="booking-item-flight-className">Check out Date
+                                                            :</p>
+                                                        <p className="booking-item-car-title">{endDate}</p>
+                                                    </div>
+                                                    <div className="starred-item__content col-sm-1">
+                                                        <p className="booking-item-flight-className labelColorBooking">
+                                                            Price :</p>
+                                                        <p className="booking-item-car-title">
+                                                            ${booking.totalAmount}</p>
+                                                    </div>
+                                                    <div className="starred-item__content col-sm-3">
+                                                        <p className="booking-item-flight-className labelColorBooking">
+                                                            Hotel Address :</p>
+                                                        <p className="booking-item-car-title">{booking.hotel.address}</p>
+                                                        <p className="booking-item-car-title">{booking.hotel.city}</p>
+                                                        <p className="booking-item-car-title">{booking.hotel.state} {booking.hotel.country}
+                                                            {booking.hotel.zipcode}</p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        );
+                    })
+                    }
                 </div>
             );
         }
